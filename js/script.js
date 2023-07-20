40f9f95679d9a41a58389b09aaa424a014b4e420
Tommy Ullberg <tommy@ullberg.net>
2023-07-20 19:16:08 +0200

    Updates

--- js/script.js
+++ js/script.js
@@ -32,30 +32,59 @@ const colorMap = new Map([
 ]);
 
 function addClassToSvgGauge(svgId, voteValue) {
-  const $ = jQuery;
-  const svgElement = document.getElementById(svgId);
-  const svgDoc = svgElement.contentDocument;
-  const svgEl = svgDoc.getElementById('gaugeSvg');
-  const colorDim = '#444444';
-  const colorFill = '#222222';
-  const colorVal = colorMap.get(Math.floor(voteValue));
-  const $borderFill = $(svgEl).find('#borderFill');
-  const $borderVal = $(svgEl).find('#borderVal');
-  const $percentVal = $(svgEl).find('#percentVal');
-  const maxGaugeValue = 490.0;
-  let decimalPercent = 0.0;
-  let wantedValue = 0.0;
-  const percentValue = voteValue * 10.0;
-  if (null !== svgEl && null !== decimalPercent) {
-    decimalPercent = percentValue / 100.0;
-    wantedValue = maxGaugeValue - decimalPercent * maxGaugeValue;
-    $borderFill.attr('stroke', colorFill);
-    $borderVal.attr('stroke', colorVal);
-    $percentVal.html(roundToDecimalIfNeeded(voteValue));
-  } else {
-    wantedValue = maxGaugeValue / 2;
-    $borderFill.add($borderVal).attr('stroke', colorDim);
-    $percentVal.html('');
+  addClassToSvgGaugeTry(svgId, voteValue, 0);
+}
+
+let oTryTimer = null,
+   svgElement = null
+
+function addClassToSvgGaugeTry(svgId, voteValue, tryCount) {
+  console.log('svgId: ' + svgId + ', voteValue: ' + voteValue + ', tryCount: ' + tryCount);
+  if (tryCount > 10) {
+    if (svgElement) {
+      svgElement.remove();
+    }
+    window.clearTimeout(oTryTimer);
+    return;
   }
-  $borderVal.attr('stroke-dashoffset', wantedValue);
+  try {
+    const $ = jQuery;
+    svgElement          = document.getElementById(svgId);
+    const svgDoc        = svgElement.contentDocument;
+    const svgEl         = svgDoc.getElementById('gaugeSvg');
+    const colorDim      = '#555555';
+    const colorFill     = '#333333';
+    const colorVal      = colorMap.get(Math.floor(voteValue));
+    const $borderFill   = $(svgEl).find('#borderFill');
+    const $borderVal    = $(svgEl).find('#borderVal');
+    const $percentVal   = $(svgEl).find('#percentVal');
+    const maxGaugeValue = 490.0;
+    let decimalPercent  = 0.0;
+    let wantedValue     = 0.0;
+    const percentValue = voteValue * 10.0;
+
+    if (null !== svgEl && null !== decimalPercent) {
+      decimalPercent = percentValue / 100.0;
+      wantedValue = maxGaugeValue - decimalPercent * maxGaugeValue;
+      $borderFill.attr('stroke', colorFill);
+      $borderVal.attr('stroke', colorVal);
+      $percentVal.html(roundToDecimalIfNeeded(voteValue));
+    } else {
+      wantedValue = maxGaugeValue / 2;
+      $borderFill.add($borderVal).attr('stroke', colorDim);
+      $percentVal.html('-');
+    }
+    $borderVal.attr('stroke-dashoffset', wantedValue);
+  } catch (err) {
+    console.error(err);
+    if (tryCount > 6) {
+      voteValue = 0;
+    }
+    if (!svgId) {
+      return;
+    }
+    oTryTimer = window.setTimeout(() => {
+      addClassToSvgGaugeTry(svgId, voteValue, tryCount + 1);
+    }, 100);
+  }
 }
